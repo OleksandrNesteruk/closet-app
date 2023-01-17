e5a6a685e85da4ca65b0c532e11595e10018a323
Oleksandr Nesteruk <54321777tv@gmail.com>
2023-01-17 12:24:43 +0200

    First commit

---
+++ src/main/java/sringboot/garderobapp/GarderobAppApplication.java
@@ -0,0 +1,13 @@
+package sringboot.garderobapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarderobAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarderobAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sringboot/garderobapp/GarderobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sringboot.garderobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarderobAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
